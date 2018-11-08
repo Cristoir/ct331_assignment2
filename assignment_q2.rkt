0960f67ca2a530abfcf2a51a39779350199490b2
Cristoir <cmullins017@gmail.com>
2018-11-08 20:19:22 +0000

    Update assignment_q2.rkt

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -12,3 +12,11 @@
 (define (ins_end el lst)
 (append lst (list el)))
 
+;part c
+(provide cout_top_level)
+
+
+(define (cout_top_level list)
+  (cond [(null? list) 0]
+  [else (+ 1 (cout_top_level (cdr list)))])
+)
